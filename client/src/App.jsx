01854e54e01702753fab45e2316df6b70874a7b3
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-01 19:29:21 +0900

    feat:homepage 메인파일추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
 import Header from "./components/Layout/Header";
 import Banner from "./components/Homepage/Banner";
 import NewsArticle from "./components/Homepage/News/NewsArticle";
 import Card from "./components/Card";
-const App=()=> {
-
-
+import Footer from "./components/Layout/Footer";
+const App = () => {
   return (
     <>
-    <Header />
-    <Banner/>
-    <NewsArticle/>
-    <Card/>
-
-
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Homepage />} />
+        </Routes>
+        <Banner />
+        <NewsArticle />
+        <Card />
+        <Footer/>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <div className="footer" style={{ paddingTop: "30px", marginTop: "40px" }}>
+      <p
+        style={{
+          fontFamily: "NeoDunggeunmo",
+          color: "#8d8d8d",
+          marginBottom: "5px",
+        }}
+      >
+        이지윤
+      </p>
+
+      <p
+        style={{
+          fontFamily: "NeoDunggeunmo",
+          color: "#8d8d8d",
+          marginBottom: "5px",
+        }}
+      >
+        이채영
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
