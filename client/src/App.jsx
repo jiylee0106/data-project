acf5a917e1a0b3439a17a75c4bbc31a1e0e9f3ed
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-01 19:42:15 +0900

    fix:router파일추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./components/Homepage/Homepage";
-import Header from "./components/Layout/Header";
-import Banner from "./components/Homepage/Banner";
-import NewsArticle from "./components/Homepage/News/NewsArticle";
-import Card from "./components/Card";
-import Footer from "./components/Layout/Footer";
+
+import Router from "./Router";
 const App = () => {
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" exact element={<Homepage />} />
-        </Routes>
-        <Banner />
-        <NewsArticle />
-        <Card />
-        <Footer/>
-      </Router>
+    <Router />
     </>
   );
 };
--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,10 +1,18 @@
 
-const Homepage = ()=>{
-  return(
+
+import Banner from "./components/Homepage/Banner";
+import NewsArticle from "./components/Homepage/News/NewsArticle";
+import Card from "./components/Card";
+
+
+const Homepage = () => {
+  return (
     <>
-    <div>homepage</div>
+        <Banner />
+        <NewsArticle />
+        <Card />
     </>
-  )
-}
+  );
+};
 
 export default Homepage;
