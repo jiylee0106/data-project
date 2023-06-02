48342952e51fedae488a45dbd515b69d86442164
이승현 <billlee2656@gmail.com>
2023-06-02 19:46:19 +0900

    feat: cors 백엔드 설정 완료

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.NODE_ENV === "dev" ? process.env.CLIENT_DEV_ORIGIN : "",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Authorization"],
+  })
+);
 
 app.get("/", async (req, res) => {
   res.send("Hello, World");
