6398311c504d5dba32a0c9293db42782eb0a030a
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-03 08:42:58 +0900

    fix:router안에header,footer

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Layout/Header";
 import Homepage from "./components/Homepage/Homepage";
 import LoginForm from "./components/User/Loginform";
 import RegisterForm from "./components/User/RegisterForm";
+import Footer from "./components/Layout/Footer";
 const Router = () => {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" exact element={<Homepage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
--- client/src/components/User/Loginform.jsx
+++ client/src/components/User/Loginform.jsx
@@ -101,9 +101,8 @@ const LoginForm = () => {
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
             disabled={!isFormValid}
-            onClick={() => navigate("/register")}
           >
             Sign In
           </button>
