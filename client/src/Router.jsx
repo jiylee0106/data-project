a1869e2d0191c72583a4b47b541f6f460e488193
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-03 09:28:46 +0900

    fix:로그인회원가입시헤더안보이게

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,16 +3,23 @@ import Header from "./components/Layout/Header";
 import Homepage from "./components/Homepage/Homepage";
 import LoginForm from "./components/User/Loginform";
 import RegisterForm from "./components/User/RegisterForm";
+import About from "./components/About/About";
+import Participate from "./components/Participate/Participate";
+import Data from "./components/Data/Data";
 import Footer from "./components/Layout/Footer";
 const Router = () => {
   return (
     <>
       <BrowserRouter>
-        <Header />
+        {location.pathname !== "/login" &&
+          location.pathname !== "/register" && <Header />}
         <Routes>
           <Route path="/" exact element={<Homepage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/articipate" exact element={<Participate />} />
+          <Route path="/data" exact element={<Data />} />
         </Routes>
         <Footer />
       </BrowserRouter>
