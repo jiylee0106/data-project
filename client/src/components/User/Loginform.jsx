9a74608a3d5ca85a87f80c90b3ade3a3a36c7c4d
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-03 09:27:58 +0900

    fix:로그인폼디자인수정

--- client/src/components/User/Loginform.jsx
+++ client/src/components/User/Loginform.jsx
@@ -45,76 +45,125 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="w-full max-w-xs">
-      <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleSubmit}
-      >
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
-          >
-            Email
-          </label>
-          <input
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            id="email"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        {!isEmailValid && email !== "" && (
-          <p className="text-red-500 text-xs italic">
-            이메일 형식이 올바르지 않습니다.
-          </p>
-        )}
-        {!isFormValid && email === "" && (
-          <p className="text-red-500 text-xs italic">이메일을 입력해주세요.</p>
-        )}
+    <div className="bg-white dark:bg-gray-900">
+      <div className="flex justify-center h-screen">
+        <div
+          className="hidden bg-cover lg:block lg:w-2/3"
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)",
+          }}
+        >
+          <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+            <div>
+              <h2 className="text-4xl font-bold text-white">시나브로</h2>
 
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            id="password"
-            type="password"
-            placeholder="******************"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-
-          {!isFormValid && password === "" && (
-            <p className="text-red-500 text-xs italic">
-              비밀번호를 입력해주세요.
-            </p>
-          )}
+              <p className="max-w-xl mt-3 text-gray-300">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
+                autem ipsa, nulla laboriosam dolores, repellendus perferendis
+                libero suscipit nam temporibus molestiae
+              </p>
+            </div>
+          </div>
         </div>
 
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-            disabled={!isFormValid}
-          >
-            Sign In
-          </button>
+        <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+          <div className="flex-1">
+            <div className="text-center">
+              <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white">
+                시나브로
+              </h2>
 
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
+              <p className="mt-3 text-gray-500 dark:text-gray-300">
+                Sign in to access your account
+              </p>
+            </div>
+
+            <div className="mt-8">
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm text-gray-600 dark:text-gray-200"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    id="email"
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                  {!isEmailValid && email !== "" && (
+                    <p className="text-red-500 text-xs italic">
+                      이메일 형식이 올바르지 않습니다.
+                    </p>
+                  )}
+                  {!isFormValid && email === "" && (
+                    <p className="text-red-500 text-xs italic">
+                      이메일을 입력해주세요.
+                    </p>
+                  )}
+                </div>
+
+                <div className="mt-6">
+                  <div className="flex justify-between mb-2">
+                    <label
+                      htmlFor="password"
+                      className="text-sm text-gray-600 dark:text-gray-200"
+                    >
+                      Password
+                    </label>
+                    <a
+                      href="#"
+                      className="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="******************"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                  {!isFormValid && password === "" && (
+                    <p className="text-red-500 text-xs italic">
+                      비밀번호를 입력해주세요.
+                    </p>
+                  )}
+                </div>
+
+                <div className="mt-6">
+                  <button
+                    type="submit"
+                    disabled={!isFormValid}
+                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-6 text-sm text-center text-gray-400">
+                Don&#x27;t have an account yet?{" "}
+                <a
+                  className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                  onClick={() => navigate("/register")}
+                >
+                  Sign up
+                </a>
+                .
+              </p>
+            </div>
+          </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
