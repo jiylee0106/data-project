c5fbe88810fdae9c66196056e357d8395aaafce1
이승현 <billlee2656@gmail.com>
2023-06-05 00:40:03 +0900

    fix: 멱등성 부분 db초기화 방식으로 수정

--- server/test/auth/auth.register.e2e-spec.ts
+++ server/test/auth/auth.register.e2e-spec.ts
@@ -1,26 +1,31 @@
 import request from "supertest";
 import { app } from "../../src";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+beforeAll(async () => {
+  await prisma.user.deleteMany();
+});
+
+afterAll(async () => {
+  await prisma.user.deleteMany();
+});
 
 describe("registerE2ETest", () => {
-  it("회원가입 성공 E2E 테스트", async () => {
+  it("회원가입 성공 및 중복 이메일 E2E 테스트", async () => {
     const res = await request(app)
       .post("/api/auth/register")
       .send({ email: "test@test.test", password: "test1234!@" });
 
     expect(res.body.token).toBeDefined();
     expect(res.statusCode).toEqual(201);
-
-    const deleteResult = await request(app)
-      .delete("/api/user")
-      .set("Authorization", `Bearer ${res.body.token}`);
-
-    expect(deleteResult.statusCode).toEqual(201);
   });
 
   it("회원가입 중복 이메일 E2E 테스트", async () => {
     const res = await request(app)
       .post("/api/auth/register")
-      .send({ email: "test@gmail.com", password: "test1234!@" });
+      .send({ email: "test@test.test", password: "test1234!@" });
 
     expect(res.body.message).toEqual("이미 존재하는 이메일입니다");
     expect(res.statusCode).toEqual(403);
