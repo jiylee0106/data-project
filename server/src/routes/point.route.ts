2b0d973f85f422218d05e19a3eb2b29a69af68e8
이승현 <billlee2656@gmail.com>
2023-06-04 20:27:31 +0900

    feat: 포인트 설계 구조 작성

--- server/src/routes/point.route.ts
+++ server/src/routes/point.route.ts
@@ -1,8 +1,11 @@
+import PointController from "@src/controllers/point.controller";
 import { Router } from "express";
+import Container from "typedi";
 
 const router = Router();
+const pointController = Container.get(PointController);
 
-router.post("/");
-router.patch("/");
+router.get("/", pointController.getPointController);
+router.put("/", pointController.earnPointController);
 
 export default router;
