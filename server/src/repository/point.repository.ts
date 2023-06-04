f7c0cf5a9b0d632428dd7b3e5c0d9877079d85b8
이승현 <billlee2656@gmail.com>
2023-06-05 01:05:33 +0900

    feat: 포인트 리포지토리 기본 골조 설정

---
+++ server/src/repository/point.repository.ts
@@ -0,0 +1,23 @@
+import { Service } from "typedi";
+import { PrismaClient, PointsLog } from "@prisma/client";
+const prisma = new PrismaClient();
+
+@Service()
+class UserRepository {
+  async getPointsLog(user_id: number): Promise<PointsLog[]> {
+    const result = await prisma.pointsLog.findMany({
+      where: { userId: user_id },
+    });
+    return result;
+  }
+
+  async putPointsLog(
+    data: Pick<PointsLog, "userId" | "points" | "action_type" | "method">
+  ) {
+    await prisma.pointsLog.create({
+      data,
+    });
+  }
+}
+
+export default UserRepository;
