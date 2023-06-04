fef4cbde249ac55f68b4b8e32d7bd56d047df885
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-05 02:24:59 +0900

    fix:lazy사용

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,26 +1,27 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Homepage from "./components/Homepage/Homepage";
-import LoginForm from "./components/User/Loginform";
-import RegisterForm from "./components/User/RegisterForm";
-import About from "./components/About/About";
-import Participate from "./components/Participate/Participate";
-import Article from "./components/Article/Article";
-import Data from "./components/Data/Data";
+const Homepage = lazy(() => import("./components/Homepage/Homepage"));
+const LoginForm = lazy(() => import("./components/User/Loginform"));
+const RegisterForm = lazy(() => import("./components/User/RegisterForm"));
+const About = lazy(() => import("./components/About/About"));
+const Participate = lazy(() => import("./components/Participate/Participate"));
+const Article = lazy(() => import("./components/Article/Article"));
+const Data = lazy(() => import("./components/Data/Data"));
 
 const Router = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" exact element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/about" exact element={<About />} />
-        <Route path="/participate" exact element={<Participate />} />
-        <Route path="/article" exact element={<Article />} />
-        <Route path="/data" exact element={<Data />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/participate" element={<Participate />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/data" element={<Data />} />
       </Routes>
-    </>
+    </Suspense>
   );
 };
 
