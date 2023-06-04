4ca6482a54c38eea0ff81a522aa7fb109b2d59b1
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-04 23:49:28 +0900

    fix:windowlocation useEffect적용

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,20 +1,30 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
 
-  if (
-    window.location.pathname === "/login" ||
-    window.location.pathname === "/register"
-  )
-    return null;
+  const location = useLocation();
+
+  const [headerVisible, setHeaderVisible] = useState(false);
+
+  useEffect(() => {
+    if (
+      window.location.pathname === "/login" ||
+      window.location.pathname === "/register"
+    ) {
+      setHeaderVisible(false);
+    } else {
+      setHeaderVisible(true);
+    }
+  }, [location.pathname]);
 
   const btnstyle =
     "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
 
   // 기본 페이지로 돌아가기
 
-  return (
+  return headerVisible ? (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a className="flex items-center">
@@ -94,7 +104,7 @@ const Header = () => {
         </div>
       </div>
     </nav>
-  );
+  ) : null;
 };
 
 export default Header;
