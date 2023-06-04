7ed5f3aaf9bfce367c2265efbf6f477b292e2317
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-04 23:30:49 +0900

    fix:header,footer app안으로

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Layout/Header";
+import { Routes, Route } from "react-router-dom";
+
 import Homepage from "./components/Homepage/Homepage";
 import LoginForm from "./components/User/Loginform";
 import RegisterForm from "./components/User/RegisterForm";
@@ -7,25 +7,19 @@ import About from "./components/About/About";
 import Participate from "./components/Participate/Participate";
 import Article from "./components/Article/Article";
 import Data from "./components/Data/Data";
-import Footer from "./components/Layout/Footer";
+
 const Router = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" exact element={<Homepage />} />
-          <Route path="/login" element={<LoginForm hideHeader />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/about" exact element={<About />} />
-          <Route path="/participate" exact element={<Participate />} />
-          <Route path="/article" exact element={<Article />} />
-
-          <Route path="/data" exact element={<Data />} />
-        </Routes>
-
-        <Footer />
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Homepage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/participate" exact element={<Participate />} />
+        <Route path="/article" exact element={<Article />} />
+        <Route path="/data" exact element={<Data />} />
+      </Routes>
     </>
   );
 };
