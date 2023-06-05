025ceb0303ca939b7866475c4964012e01e19449
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-05 16:04:58 +0900

    fix:buttontype 변경

--- client/src/components/User/RegisterForm.jsx
+++ client/src/components/User/RegisterForm.jsx
@@ -179,7 +179,7 @@ const RegisterForm = () => {
                   </div>
                 </div>
                 <button
-                  type="button"
+                  type="submit"
                   disabled={!isFormValid}
                   className="bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-blue-200"
                 >
