c9d0cdf89271495d980e541ab97880333f1c3e62
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-06 02:22:55 +0900

    fix:불필요한파일삭제,토큰undefined해결

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const serverUrl = import.meta.env.VITE_SERVER_HOST;
 
 const getToken = () => {
-  return sessionStorage.getItem("accessToken");
+  return sessionStorage.getItem("accessToken") || undefined;
 };
 
 const instance = axios.create({
