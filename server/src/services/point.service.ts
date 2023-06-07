cd0e2729a555b75a98e16396e5b2424dc19dfc15
이승현 <billlee2656@gmail.com>
2023-06-07 22:45:54 +0900

    feat: 캠페인 로그 및 일일 로그 서비스 구현

--- server/src/services/point.service.ts
+++ server/src/services/point.service.ts
@@ -1,13 +1,23 @@
 import { PointsLog } from "@prisma/client";
 import HandlePoint from "@src/libraries/integrations/handlePoints";
+import PointRepository from "@src/repository/point.repository";
 import { Inject, Service } from "typedi";
 
 @Service()
 class PointService {
   @Inject() private readonly handlePoint: HandlePoint;
+  @Inject() private readonly pointRepository: PointRepository;
 
   async getPointService(user_id: number): Promise<number> {
     return await this.handlePoint.getPoints(user_id);
+  }
+
+  async getCampaignLogService(user_id: number): Promise<PointsLog[]> {
+    return await this.pointRepository.getCampaignLog(user_id);
+  }
+
+  async getDailyEventsLogService(user_id: number): Promise<PointsLog[]> {
+    return await this.pointRepository.getDailyEventsLog(user_id);
   }
 
   async putPointService(
