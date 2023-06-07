cd5eae67bca205114192621331592d3d3f113ba3
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-07 14:36:15 +0900

    fix:사소한코드수정

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -209,11 +209,7 @@ const Header = () => {
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                onClick={() => navigate("/")}
-                className={btnstyle}
-                aria-current="page"
-              >
+              <a onClick={() => navigate("/")} className={btnstyle}>
                 홈
               </a>
             </li>
