49a91e4c6efe17c5bb4459a6088711a7212abf49
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-08 19:21:37 +0900

    feat:퀴즈폼추가

--- client/src/components/Participate/Participate.jsx
+++ client/src/components/Participate/Participate.jsx
@@ -1,7 +1,18 @@
+import Quiz from "./Quiz";
+
 const Participate = () => {
   return (
     <>
-      <p>참여페이지</p>
+      <div className="m-20 p-20 bg-white flex flex-col lg:flex-row">
+        <div className="w-full lg:w-1/2 p-10">
+          <Quiz />
+        </div>
+        <div className="w-full lg:w-1/2 p-10">
+          <div className="md:h-full md:h-auto p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <img src="images/polarbear.png" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
