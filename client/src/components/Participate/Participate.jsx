60df49bbe6e02f65eb8790e1ca0b6719019942cf
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-08 19:57:01 +0900

    fix:퀴즈폼레이아웃수정

--- client/src/components/Participate/Participate.jsx
+++ client/src/components/Participate/Participate.jsx
@@ -1,19 +1,17 @@
+import Campaign from "./Campaign/Campaign";
 import Quiz from "./Quiz";
 
 const Participate = () => {
   return (
-    <>
-      <div className="m-20 p-20 bg-white flex flex-col lg:flex-row">
-        <div className="w-full lg:w-1/2 p-10">
-          <Quiz />
-        </div>
-        <div className="w-full lg:w-1/2 p-10">
-          <div className="md:h-full md:h-auto p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <img src="images/polarbear.png" />
-          </div>
-        </div>
+    <div className="m-8 p-10 bg-white flex flex-col">
+      <div className="w-full">
+        <Campaign />
       </div>
-    </>
+
+      <div className="w-full flex-row">
+        <Quiz />
+      </div>
+    </div>
   );
 };
 
