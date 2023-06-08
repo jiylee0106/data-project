2f074ff895f3082c57854c17c1272a80fa5fdd73
Jun <shabagshao@gmail.com>
2023-06-09 01:27:50 +0900

    feat: 탭 기능 추가

--- client/src/components/About/Intent.jsx
+++ client/src/components/About/Intent.jsx
@@ -1,14 +1,60 @@
+import { useState } from 'react';
+
 const Intent = () => {
+  const [activeTab, setActiveTab] = useState("profile");
+
+  const toggleTab = (tabId) => {
+    setActiveTab(tabId === activeTab ? null : tabId);
+  };
+
   return (
     <div className="mt-14 py-4">
       <h2 className="text-3xl font-bold mb-2">시나브로와 함께해요!</h2>
       <div className="flex items-center">
-        <img
-          src="https://cdn.discordapp.com/attachments/1114069039757676599/1114075473459302410/FxcOYbmacAA10-p.jpg"
-          alt="기획 의도 이미지"
-          className="w-1/4 lg:w-1/3 mr-4"
-        />
-        <p className="text-lg">기획 의도에 대한 설명입니다.</p>
+        <div className="w-1/4 lg:w-1/3 mr-4">
+          <img
+            src="https://cdn.discordapp.com/attachments/1114069039757676599/1114075473459302410/FxcOYbmacAA10-p.jpg"
+            alt="기획 의도 이미지"
+            className="rounded-lg max-w-full"
+          />
+        </div>
+        <div>
+          <p className="text-lg">기획 의도에 대한 설명입니다.</p>
+          <div className="flex justify-end mt-2">
+            <button className={`focus:outline-none mr-2 ${activeTab === 'profile' ? 'text-blue-600' : 'text-gray-600 hover:text-blue-600'}`} onClick={() => toggleTab('profile')}>
+              시나브로
+            </button>
+            <button className={`focus:outline-none mr-2 ${activeTab === 'likes' ? 'text-blue-600' : 'text-gray-600 hover:text-blue-600'}`} onClick={() => toggleTab('likes')}>
+              좋아요
+            </button>
+            <button className={`focus:outline-none mr-2 ${activeTab === 'collection' ? 'text-blue-600' : 'text-gray-600 hover:text-blue-600'}`} onClick={() => toggleTab('collection')}>
+              컬렉션
+            </button>
+          </div>
+          <div className="mt-2">
+            {activeTab === 'profile' && (
+              <div className="p-4 rounded-lg bg-gray-50 dark:bg-gray-800">
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  기획 의도에 대한 설명입니다.
+                </p>
+              </div>
+            )}
+            {activeTab === 'likes' && (
+              <div className="p-4 rounded-lg bg-gray-50 dark:bg-gray-800">
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  좋아요에 대한 설명입니다.
+                </p>
+              </div>
+            )}
+            {activeTab === 'collection' && (
+              <div className="p-4 rounded-lg bg-gray-50 dark:bg-gray-800">
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  컬렉션에 대한 설명입니다.
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/About/SpeciesRank.jsx
+++ client/src/components/About/SpeciesRank.jsx
@@ -8,12 +8,17 @@ const SpeciesRank = () => {
           <img
             src="https://cdn.discordapp.com/attachments/1114069039757676599/1114075473459302410/FxcOYbmacAA10-p.jpg"
             alt="멸종 위기 1급 이미지"
-            className="rounded-lg"
+            className="rounded-lg max-w-full"
           />
         </div>
         <div className="w-3/4">
-          <h3 className="text-xl font-bold mb-2">멸종 위기 1급에 대한 설명</h3>
-          <p className="text-lg">멸종 위기 1급에 대한 상세 설명입니다.</p>
+          <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
+            <h3 className="text-xl font-bold mb-2">멸종 위기종 1급</h3>
+            <p className="text-lg">
+              자연적 또는 인위적 위협요인으로 개체 수가<br />
+              많이 줄어들어 멸종위기에 처한 야생생물
+            </p>
+          </div>
         </div>
       </div>
 
@@ -22,12 +27,18 @@ const SpeciesRank = () => {
           <img
             src="https://cdn.discordapp.com/attachments/1114069039757676599/1114075473459302410/FxcOYbmacAA10-p.jpg"
             alt="멸종 위기 2급 이미지"
-            className="rounded-lg"
+            className="rounded-lg max-w-full"
           />
         </div>
         <div className="w-3/4">
-          <h3 className="text-xl font-bold mb-2">멸종 위기 2급에 대한 설명</h3>
-          <p className="text-lg">멸종 위기 2급에 대한 상세 설명입니다.</p>
+          <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
+            <h3 className="text-xl font-bold mb-2">멸종 위기종 2급</h3>
+            <p className="text-lg">
+              자연적 또는 인위적 위협요인으로 개체 수가<br />
+              크게 줄어들고 있어 현재의 위협요인이 제거되거나 완화되지 아니할 경우<br /> 
+              가까운 장래에 멸종위기에 처할 우려가 있는 야생생물
+            </p>
+          </div>
         </div>
       </div>
     </div>
