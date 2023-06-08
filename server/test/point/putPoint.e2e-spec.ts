b31cb7e74cb05d75832a48927223f4b44d931ae0
이승현 <billlee2656@gmail.com>
2023-06-08 15:23:36 +0900

    feat: 포인트 관련 테스트 구축

---
+++ server/test/point/putPoint.e2e-spec.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import { app } from "../../src";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+let token: string;
+
+beforeAll(async () => {
+  await prisma.collection.deleteMany();
+  await prisma.pointsLog.deleteMany();
+  await prisma.user.deleteMany();
+  const user = await request(app)
+    .post("/api/auth/register")
+    .send({ email: "test@gmail.com", password: "test1234!@" });
+
+  token = user.body.token;
+});
+
+afterAll(async () => {
+  await prisma.collection.deleteMany();
+  await prisma.pointsLog.deleteMany();
+  await prisma.user.deleteMany();
+});
+
+describe("getPointE2ETest", () => {
+  it("포인트 적립 성공 E2E 테스트", async () => {
+    const res = await request(app)
+      .put("/api/point")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ action_type: "Earned", method: "Watched_Data" });
+
+    expect(res.body.message).toEqual("포인트 적립에 성공하였습니다");
+    expect(res.statusCode).toEqual(201);
+  });
+
+  it("포인트 적립 후 사용 성공 E2E 테스트", async () => {
+    await request(app)
+      .put("/api/point")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ action_type: "Earned", method: "Joined_Campaign1" });
+
+    const res = await request(app)
+      .put("/api/point")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ action_type: "Spent", method: "Draw_Degree2" });
+
+    expect(res.body.message).toEqual("포인트 사용에 성공하였습니다");
+    expect(res.statusCode).toEqual(201);
+  });
+});
