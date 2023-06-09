ad92b8d661a744016bd291a2303903e4d1244a47
이승현 <billlee2656@gmail.com>
2023-06-10 04:10:20 +0900

    feat: 프론트 자료탭 논리 변경

--- client/src/components/Data/ReasonData/ReasonData.jsx
+++ client/src/components/Data/ReasonData/ReasonData.jsx
@@ -2,6 +2,7 @@ import ScatterChart from "../Chart/ScatterChart";
 import { nationalParkData } from "../../../data/national_park_data";
 import { locateEndangerData } from "../../../data/locate_endanger_data";
 import { endangerYearData } from "../../../data/endanger_year_data";
+import { ecologicalPathway } from "../../../data/ecological_pathway_data";
 import LineChart from "../Chart/LineChart";
 
 const ReasonData = () => {
@@ -15,6 +16,17 @@ const ReasonData = () => {
             title: "멸종위기종과 국립공원 수의 상관관계",
             xLabel: "지역별 국립공원 수",
             yLabel: "지역별 멸종 위기종 수",
+          }}
+        />
+      </div>
+      <div className="my-10">
+        <ScatterChart
+          xData={ecologicalPathway}
+          yData={locateEndangerData}
+          description={{
+            title: "zz",
+            xLabel: "awrer",
+            yLabel: "awer",
           }}
         />
       </div>
