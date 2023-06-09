da182814a13d737bc9f833f5eeddc27622ba2870
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-09 21:13:49 +0900

    feat:동참하기탭구성완료

--- client/src/components/Participate/Join/Join.jsx
+++ client/src/components/Participate/Join/Join.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
 import JoinDescr from "./JoinDescr";
 import JoinImage from "./JoinImage";
+import * as Api from "../../../services/api";
+
+const joinData = { title: "Join1", description: "join설명" };
 
 const Join = () => {
+  const [joinLogs, setJoinLogs] = useState([]);
+  const [joinStatus, setJoinStatus] = useState(false);
+
+  const [participateStatus, setParticipateStatus] = useState(0);
+
+  useEffect(() => {
+    getJoinLogs();
+  }, [participateStatus]);
+
+  useEffect(() => {
+    joinLogs.forEach((item) => {
+      if (item.method === "Participation") {
+        setJoinStatus(true);
+      }
+    });
+  }, [joinLogs]);
+
+  const getJoinLogs = async () => {
+    try {
+      const response = await Api.get("point/daily-events");
+
+      setJoinLogs(response.data.logs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-10 bg-white flex flex-col lg:flex-row">
       <div className="w-full p-6 lg:w-1/2 justify-items-center ">
@@ -9,7 +40,13 @@ const Join = () => {
       </div>
 
       <div className="w-full p-6 lg:w-1/2">
-        <JoinDescr />
+        <JoinDescr
+          participateStatus={participateStatus}
+          setParticipateStatus={setParticipateStatus}
+          status={joinStatus}
+          title={joinData.title}
+          description={joinData.description}
+        />
       </div>
     </div>
   );
---
+++ client/src/components/Participate/Join/JoinCheck.jsx
@@ -0,0 +1,30 @@
+import * as Api from "../../../services/api";
+
+const JoinCheck = ({ participateStatus, setParticipateStatus, status }) => {
+  const handleParticipate = async () => {
+    try {
+      await Api.put("point", {
+        action_type: "Earned",
+        method: "Participation",
+      });
+
+      setParticipateStatus(participateStatus + 1); // method 값을 배열에 추가
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <button
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={handleParticipate}
+        disabled={status}
+      >
+        {status ? "참여 완료" : "동참하기"}
+      </button>
+    </div>
+  );
+};
+
+export default JoinCheck;
