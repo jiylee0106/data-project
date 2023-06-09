882bc80f6244d02f3f84718b87bdf0c8db7f0aef
이승현 <billlee2656@gmail.com>
2023-06-09 22:43:21 +0900

    feat: 산점도 차트 구색 완성

--- client/src/components/Data/Chart/ScatterChart.jsx
+++ client/src/components/Data/Chart/ScatterChart.jsx
@@ -10,15 +10,26 @@ import { Scatter } from "react-chartjs-2";
 
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
-const options = {
-  scales: {
-    y: {
-      beginAtZero: true,
+const ScatterChart = ({ xData, yData, description }) => {
+  const options = {
+    scales: {
+      x: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: description.xLabel,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: description.yLabel,
+        },
+      },
     },
-  },
-};
+  };
 
-const ScatterChart = ({ xData, yData }) => {
   const reshapedData = xData.y.map((value, index) => ({
     x: value,
     y: yData.y[index],
@@ -28,7 +39,7 @@ const ScatterChart = ({ xData, yData }) => {
     labels: xData.x,
     datasets: [
       {
-        label: "Number of Endangered Species vs National Parks",
+        label: description.title,
         data: reshapedData,
         backgroundColor: "rgba(255, 99, 132, 0.5)",
         borderColor: "rgba(255, 99, 132, 1)",
@@ -38,8 +49,10 @@ const ScatterChart = ({ xData, yData }) => {
   };
 
   return (
-    <div className="w-[50%]">
-      <Scatter options={options} data={data} />
+    <div className="flex justify-center">
+      <div className="w-[50%]">
+        <Scatter options={options} data={data} />
+      </div>
     </div>
   );
 };
--- client/src/components/Data/ReasonData/ReasonData.jsx
+++ client/src/components/Data/ReasonData/ReasonData.jsx
@@ -5,7 +5,15 @@ import { locateEndangerData } from "../../../data/locate_endanger_data";
 const ReasonData = () => {
   return (
     <>
-      <ScatterChart xData={nationalParkData} yData={locateEndangerData} />
+      <ScatterChart
+        xData={nationalParkData}
+        yData={locateEndangerData}
+        description={{
+          title: "멸종위기종과 국립공원 수의 상관관계",
+          xLabel: "지역별 국립공원 수",
+          yLabel: "지역별 멸종 위기종 수",
+        }}
+      />
     </>
   );
 };
