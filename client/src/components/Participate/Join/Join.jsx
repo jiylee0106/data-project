56b772d5f659532b2e3104054a4697d1655f696c
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-10 03:30:34 +0900

    feat:퀴즈기능구현

--- client/src/components/Participate/Join/Join.jsx
+++ client/src/components/Participate/Join/Join.jsx
@@ -3,7 +3,11 @@ import JoinDescr from "./JoinDescr";
 import JoinImage from "./JoinImage";
 import * as Api from "../../../services/api";
 
-const joinData = { title: "Join1", description: "join설명" };
+const joinData = {
+  title: "자가용 대신 대중교통이용",
+  description:
+    "자가용 대신 대중교통을 이용하면 연간 17.4그루의 소나무를 지구에 심는 효과를 낼 수 있어요!승용차 1대당 1년에 약 2톤의 CO2를 배출합니다. 현재 국내에 약 2,500만대 이상의 자동차가 등록되어 있다고 하니,가히 천문학적인 수치의 온실가스를 마구 배출하고 있는 셈이죠. 우리에겐 전기차라는 훌륭한 대안이 있지만,당장 바꾸기가 어렵다면 대중교통이라는 선택지가 있어요!버스는 자가용에서 배출되는 온실가스의 양의 1/3배,지하철은 1/8배까지 줄일 수 있죠.더이상 나중으로 미루지 말고, 바로 오늘부터 실천해보는 건 어떨까요.",
+};
 
 const Join = () => {
   const [joinLogs, setJoinLogs] = useState([]);
--- client/src/components/Participate/Join/JoinDescr.jsx
+++ client/src/components/Participate/Join/JoinDescr.jsx
@@ -9,13 +9,13 @@ const JoinDescr = ({
 }) => {
   return (
     <>
-      <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-7/12 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-8 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
         <JoinCheck
