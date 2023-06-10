8575a5114f81e2fcfd20d49ce4c4c5ab5ed388e3
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-10 19:23:04 +0900

    fix:멘트수정

--- client/src/components/Participate/Quiz/Quiz.jsx
+++ client/src/components/Participate/Quiz/Quiz.jsx
@@ -94,7 +94,7 @@ const QuizProblem = ({
         퀴즈 참여하기
       </h2>
       <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
-        이 동물의 이름은?
+        이 멸종위기 야생생물의 이름은?
       </h3>
 
       <ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
