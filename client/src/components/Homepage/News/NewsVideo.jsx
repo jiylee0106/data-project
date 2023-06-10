3b19c39c40a2167d05b861bafadbdd856ad46d76
[AI7/2반]이지윤 <little8867@gmail.com>
2023-06-11 01:24:33 +0900

    feat:비디오시청완료시포인트증가

--- client/src/components/Homepage/News/NewsVideo.jsx
+++ client/src/components/Homepage/News/NewsVideo.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
 import YouTube from "react-youtube";
+import * as Api from "../../../services/api";
+import Modal from "../../Modal/Modal";
 
 const NewsVideo = () => {
-  const handleVideoEnd = () => {
-    console.log("영상이 종료되었습니다.");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [videoLogs, setVideoLogs] = useState([]);
+  const [videoStatus, setVideoStatus] = useState(false);
+  const [participateStatus, setParticipateStatus] = useState(0);
+
+  useEffect(() => {
+    getVideoLogs();
+  }, [participateStatus]);
+
+  useEffect(() => {
+    videoLogs.forEach((item) => {
+      if (item.method === "Watched_Video") {
+        setVideoStatus(true);
+      }
+    });
+  }, [videoLogs]);
+
+  const getVideoLogs = async () => {
+    try {
+      const response = await Api.get("point/daily-events");
+      setVideoLogs(response.data.logs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleComplete = async () => {
+    setIsModalOpen(false);
+    try {
+      await Api.put("point", {
+        action_type: "Earned",
+        method: "Watched_Video",
+      });
+
+      setParticipateStatus(participateStatus + 1);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const videoId = "u9V5MBqhLRc"; // 재생할 영상의 유튜브 ID를 지정합니다.
@@ -20,8 +59,35 @@ const NewsVideo = () => {
   };
 
   return (
-    <div className="md:h-full md:h-auto border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <YouTube videoId={videoId} opts={opts} onEnd={handleVideoEnd} />
+    <div>
+      <div className="md:h-full md:h-auto border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <YouTube
+          videoId={videoId}
+          opts={opts}
+          onEnd={() => setIsModalOpen(true)}
+        />
+
+        <div
+          className={`inline-flex items-center mt-3 px-3 py-2 text-sm font-large text-center text-white rounded-lg focus:ring-4 focus:outline-none dark:focus:ring-blue-800 ${
+            videoStatus
+              ? "bg-blue-700 dark:bg-blue-600 dark:hover:bg-blue-700"
+              : "bg-red-700 dark:bg-red-600 dark:hover:bg-red-700"
+          }`}
+        >
+          {videoStatus ? "시청 완료" : "시청 전"}
+        </div>
+      </div>
+      {isModalOpen && (
+        <Modal
+          buttonText="확인"
+          closeModal={() => setIsModalOpen(false)}
+          handleAction={handleComplete}
+        >
+          <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+            영상시청이 완료되었습니다.
+          </h3>
+        </Modal>
+      )}
     </div>
   );
 };
