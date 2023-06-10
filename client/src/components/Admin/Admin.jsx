d3768c47d73bbab627445908f4da578f5a578073
이승현 <billlee2656@gmail.com>
2023-06-10 22:52:15 +0900

    feat: 탭 배열화 및 반복문 적용

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -5,9 +5,12 @@ import AdminTab from "./Tab/AdminTab";
 const Admin = () => {
   const [tab, setTab] = useState(0);
   return (
-    <div className="my-20 flex flex-row">
-      <AdminTab setTab={setTab} />
-      <AdminContent tab={tab} />
+    <div className="my-20">
+      <h1 className="text-center text-2xl font-bold my-10">관리자 페이지</h1>
+      <div className="flex flex-row">
+        <AdminTab setTab={setTab} />
+        <AdminContent tab={tab} />
+      </div>
     </div>
   );
 };
--- client/src/components/Admin/Tab/AdminTab.jsx
+++ client/src/components/Admin/Tab/AdminTab.jsx
@@ -1,27 +1,33 @@
+const tabList = [
+  {
+    id: 0,
+    title: "소식 관리",
+  },
+  {
+    id: 1,
+    title: "영상 관리",
+  },
+  {
+    id: 2,
+    title: "캠페인 관리",
+  },
+  {
+    id: 3,
+    title: "동참 관리",
+  },
+];
+
 const AdminTab = ({ setTab }) => {
   return (
     <div className="border-r h-screen p-5">
       <ul>
-        <li>
-          <button className="px-3 py-2" onClick={() => setTab(0)}>
-            소식 관리
-          </button>
-        </li>
-        <li>
-          <button className="px-3 py-2" onClick={() => setTab(1)}>
-            영상 관리
-          </button>
-        </li>
-        <li>
-          <button className="px-3 py-2" onClick={() => setTab(2)}>
-            캠페인 관리
-          </button>
-        </li>
-        <li>
-          <button className="px-3 py-2" onClick={() => setTab(3)}>
-            동참 관리
-          </button>
-        </li>
+        {tabList.map((item) => (
+          <li className="hover:bg-neutral-100 rounded-xl my-5" key={item.id}>
+            <button className="px-3 py-2" onClick={() => setTab(item.id)}>
+              {item.title}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
