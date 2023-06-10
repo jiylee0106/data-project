b5770e88be6d651d244b08c56b2e0ce64da6ec4e
이승현 <billlee2656@gmail.com>
2023-06-11 02:01:38 +0900

    feat: 어드민 뉴스 리포지토리 및 서비스 재구성 완료

--- server/src/repository/admin/news.repository.ts
+++ server/src/repository/admin/news.repository.ts
@@ -9,6 +9,21 @@ class NewsRepository {
   ): Promise<void> {
     await prisma.news.create({ data: news });
   }
+
+  async patchNews(
+    id: number,
+    data: Pick<News, "title" | "description" | "link" | "image_link">
+  ): Promise<void> {
+    const { title, description, link, image_link } = data;
+    await prisma.news.update({
+      where: { id },
+      data: { title, description, link, image_link },
+    });
+  }
+
+  async deleteNews(id: number): Promise<void> {
+    await prisma.news.delete({ where: { id } });
+  }
 }
 
 export default NewsRepository;
