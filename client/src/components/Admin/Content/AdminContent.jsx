ee57a31cfdf69351afb8da9adcb514975b81473a
이승현 <billlee2656@gmail.com>
2023-06-10 23:49:11 +0900

    feat: 어드민 페이지 리스트 테이블 생성

--- client/src/components/Admin/Content/AdminContent.jsx
+++ client/src/components/Admin/Content/AdminContent.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { get } from "../../../services/api";
+import AdminNews from "./Table/AdminNews";
+import AdminVideo from "./Table/AdminVideo";
+import AdminCampaign from "./Table/AdminCampaign";
+import AdminParticipation from "./Table/AdminParticipation";
 
 const AdminContent = ({ tab }) => {
   const [list, setList] = useState(null);
@@ -36,13 +40,18 @@ const AdminContent = ({ tab }) => {
   }, [list]);
 
   return (
-    <div className="ml-10">
+    <div className="ml-10 w-full">
       <div>
         <button className="border px-3 py-2 rounded mb-5 hover:bg-neutral-100">
           추가하기
         </button>
       </div>
-      <div></div>
+      <div className="w-full">
+        {(tab === 0 && <AdminNews list={list} />) ||
+          (tab === 1 && <AdminVideo list={list} />) ||
+          (tab === 2 && <AdminCampaign list={list} />) ||
+          (tab === 3 && <AdminParticipation list={list} />)}
+      </div>
     </div>
   );
 };
--- client/src/components/Admin/Content/Table/AdminNews.jsx
+++ client/src/components/Admin/Content/Table/AdminNews.jsx
@@ -1,8 +1,8 @@
-const AdminNews = () => {
+const AdminNews = ({ list }) => {
   return (
     <>
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
             <th scope="col" className="px-6 py-3">
               제목
@@ -11,16 +11,47 @@ const AdminNews = () => {
               내용
             </th>
             <th scope="col" className="px-6 py-3">
-              링크
+              뉴스 링크
             </th>
             <th scope="col" className="px-6 py-3">
               이미지 링크
             </th>
             <th scope="col" className="px-6 py-3">
-              영상 ID
+              생성 날짜
+            </th>
+            <th scope="col" className="px-6 py-3">
+              액션
             </th>
           </tr>
         </thead>
+        <tbody>
+          {list.map((item) => (
+            <tr key={item.id} className="bg-white border-b">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+              >
+                {item.title}
+              </th>
+              <td className="px-6 py-4">{item.description}</td>
+              <td className="px-6 py-4">{item.link}</td>
+              <td className="px-6 py-4">{item.image_link}</td>
+              <td className="px-6 py-4">
+                {" "}
+                {new Date(item.created_at).toLocaleDateString()}{" "}
+                {new Date(item.created_at).toLocaleTimeString()}
+              </td>
+              <td className="px-6 py-4">
+                <button className="font-medium text-blue-400 hover:underline mr-3">
+                  수정
+                </button>
+                <button className="font-medium text-red-400 hover:underline">
+                  삭제
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
