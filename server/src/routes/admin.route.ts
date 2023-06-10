3c51c1608f6f9022cfb5aa7d477d05bb03a18a69
이승현 <billlee2656@gmail.com>
2023-06-10 17:49:59 +0900

    feat: 어드민 라우터 dto 미들웨어 설정

--- server/src/routes/admin.route.ts
+++ server/src/routes/admin.route.ts
@@ -1,18 +1,56 @@
 import { Router } from "express";
 import { Container } from "typedi";
 import AdminController from "@src/controllers/admin.controller";
+import { validateBody } from "@src/middlewares/validateDto";
+import {
+  PatchCampaignRequestDto,
+  PatchParticipationRequestDto,
+  PatchVideoRequestDto,
+  PutCampaignRequestDto,
+  PutNewsRequestDto,
+  PutParticipationRequestDto,
+  PutVideoRequestDto,
+} from "@src/dto/admin.dto";
 
 const router = Router();
 const adminController = Container.get(AdminController);
 
-router.post("/news", adminController.putNewsController);
-router.post("/video", adminController.putVideoController);
-router.post("/participation", adminController.putParticipationController);
-router.post("/campaign", adminController.putCampaignController);
+router.post(
+  "/news",
+  validateBody(PutNewsRequestDto),
+  adminController.putNewsController
+);
+router.post(
+  "/video",
+  validateBody(PutVideoRequestDto),
+  adminController.putVideoController
+);
+router.post(
+  "/participation",
+  validateBody(PutParticipationRequestDto),
+  adminController.putParticipationController
+);
+router.post(
+  "/campaign",
+  validateBody(PutCampaignRequestDto),
+  adminController.putCampaignController
+);
 
-router.patch("/campaign", adminController.patchCampaignController);
-router.patch("/video", adminController.patchVideoController);
-router.patch("/participation", adminController.patchParticipationController);
+router.patch(
+  "/campaign",
+  validateBody(PatchCampaignRequestDto),
+  adminController.patchCampaignController
+);
+router.patch(
+  "/video",
+  validateBody(PatchVideoRequestDto),
+  adminController.patchVideoController
+);
+router.patch(
+  "/participation",
+  validateBody(PatchParticipationRequestDto),
+  adminController.patchParticipationController
+);
 
 router.get("/news", adminController.getNewsController);
 router.get("/video", adminController.getVideoController);
