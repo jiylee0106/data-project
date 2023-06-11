48bcff6321fa93db5c9f4c588e05bd9428a3df87
Chaeyoung <codudghkdlxld@gmail.com>
2023-06-11 20:08:50 +0900

    feat. Card에 data 연결

--- client/src/components/Homepage/DailySpecies.jsx
+++ client/src/components/Homepage/DailySpecies.jsx
@@ -1,75 +1,76 @@
 import { useEffect, useState } from "react";
+import { dataSet } from "../../data/data";
 import Card from "../Global/Card";
 import * as Api from "../../services/api";
 
-const list = [
-  {
-    id: 1,
-    name: "수달",
-    region: ["강원", "경남"],
-    degree: 1,
-    species: "포유류",
-    imageLink:
-      "https://media.discordapp.net/attachments/1114069039757676599/1114084929794482176/dab60569596261c385ba8e401315566e.jpg?width=661&height=662",
-    link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=7",
-  },
-  {
-    id: 2,
-    name: "여우",
-    region: ["충북", "경남"],
-    degree: 1,
-    species: "포유류",
-    imageLink:
-      "https://i.namu.wiki/i/mIi4JlIMiy7JpSaeG2samFF8CiAAPSP_aDpvCTtalV9kK7tfzUt09KOUodp-Pw7rmLFEdcVsadcIN4lehvgTKg.webp",
-    link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=9",
-  },
-  {
-    id: 3,
-    name: "늑대",
-    region: ["북한"],
-    degree: 1,
-    species: "포유류",
-    imageLink: "https://img.sbs.co.kr/newimg/news/20200209/201400647_700.jpg",
-    link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=1",
-  },
-  {
-    id: 4,
-    name: "눈사람",
-    region: ["서울"],
-    degree: 2,
-    species: "파충류",
-    imageLink:
-      "https://cdn.discordapp.com/attachments/1114069039757676599/1114075473459302410/FxcOYbmacAA10-p.jpg",
-    link: "https://namu.wiki/w/%EB%88%88%EC%82%AC%EB%9E%8C",
-  },
-  {
-    id: 5,
-    name: "하늘다람쥐",
-    region: ["강원"],
-    degree: 2,
-    species: "포유류",
-    imageLink:
-      "https://cdn.univ20.com/wp-content/uploads/2015/07/b3f4c1f2d3ab564d4901cdf151fe64cc.jpg",
-    link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=20",
-  },
-];
+// const list = [
+//   {
+//     id: 1,
+//     name: "수달",
+//     region: ["강원", "경남"],
+//     degree: 1,
+//     species: "포유류",
+//     imageLink:
+//       "https://media.discordapp.net/attachments/1114069039757676599/1114084929794482176/dab60569596261c385ba8e401315566e.jpg?width=661&height=662",
+//     link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=7",
+//   },
+//   {
+//     id: 2,
+//     name: "여우",
+//     region: ["충북", "경남"],
+//     degree: 1,
+//     species: "포유류",
+//     imageLink:
+//       "https://i.namu.wiki/i/mIi4JlIMiy7JpSaeG2samFF8CiAAPSP_aDpvCTtalV9kK7tfzUt09KOUodp-Pw7rmLFEdcVsadcIN4lehvgTKg.webp",
+//     link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=9",
+//   },
+//   {
+//     id: 3,
+//     name: "늑대",
+//     region: ["북한"],
+//     degree: 1,
+//     species: "포유류",
+//     imageLink: "https://img.sbs.co.kr/newimg/news/20200209/201400647_700.jpg",
+//     link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=1",
+//   },
+//   {
+//     id: 4,
+//     name: "눈사람",
+//     region: ["서울"],
+//     degree: 2,
+//     species: "파충류",
+//     imageLink:
+//       "https://cdn.discordapp.com/attachments/1114069039757676599/1114075473459302410/FxcOYbmacAA10-p.jpg",
+//     link: "https://namu.wiki/w/%EB%88%88%EC%82%AC%EB%9E%8C",
+//   },
+//   {
+//     id: 5,
+//     name: "하늘다람쥐",
+//     region: ["강원"],
+//     degree: 2,
+//     species: "포유류",
+//     imageLink:
+//       "https://cdn.univ20.com/wp-content/uploads/2015/07/b3f4c1f2d3ab564d4901cdf151fe64cc.jpg",
+//     link: "https://www.nie.re.kr/nie/pgm/edSpecies/view.do?menuNo=200121&speciesSn=20",
+//   },
+// ];
 
 const DailySpecies = () => {
-  const getRandomSpecies = (list, count) => {
-    const speciesCount = list.length;
+  const getRandomSpecies = (dataSet, count) => {
+    const speciesCount = dataSet.length;
     const selectedIds = new Set();
 
     while (selectedIds.size < count) {
       const randomIndex = Math.floor(Math.random() * speciesCount);
-      const randomId = list[randomIndex].id;
+      const randomId = dataSet[randomIndex].id;
       selectedIds.add(randomId);
     }
 
-    const selectedSpecies = list.filter((item) => selectedIds.has(item.id));
+    const selectedSpecies = dataSet.filter((item) => selectedIds.has(item.id));
     return selectedSpecies;
   };
 
-  const selectedSpecies = getRandomSpecies(list, 4);
+  const selectedSpecies = getRandomSpecies(dataSet, 4);
 
   const [speciesLogs, setSpeciesLogs] = useState([]);
   const [speciesStatus, setSpeciesStatus] = useState({
@@ -142,11 +143,12 @@ const DailySpecies = () => {
         {selectedSpecies.map((item, index) => (
           <div key={item.id} onClick={() => handleSpecies(index + 1)}>
             <Card
+              id={item.id}
               name={item.name}
               region={item.region}
               degree={item.degree}
               species={item.species}
-              imageLink={item.imageLink}
+              imageLink={`endangered/${item.id}.jpg`}
               link={item.link}
             />
             <a
