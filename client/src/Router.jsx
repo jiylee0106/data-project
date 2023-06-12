6f164cab6cdcbbf07586ac8547f5e36c3be4654f
이승현 <billlee2656@gmail.com>
2023-06-12 19:16:03 +0900

    feat: 에러바운더리 처리

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Collection from "./components/Collection/Collection";
 import Admin from "./components/Admin/Admin";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Homepage = lazy(() => import("./components/Homepage/Homepage"));
 const LoginForm = lazy(() => import("./components/User/Loginform"));
@@ -18,25 +19,27 @@ const Router = () => {
   const location = useLocation();
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <TransitionGroup>
-        <CSSTransition key={location.key} classNames="fade" timeout={500}>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/participate" element={<Participate />} />
-            <Route path="/article" element={<Article />} />
-            <Route path="/data" element={<Data />} />
-            <Route path="/change-password" element={<PwChange />} />
-            <Route path="/points" element={<Points />} />
-            <Route path="/collection" element={<Collection />} />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </CSSTransition>
-      </TransitionGroup>
-    </Suspense>
+    <ErrorBoundary FallbackComponent={<div>Error...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TransitionGroup>
+          <CSSTransition key={location.key} classNames="fade" timeout={500}>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<RegisterForm />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/participate" element={<Participate />} />
+              <Route path="/article" element={<Article />} />
+              <Route path="/data" element={<Data />} />
+              <Route path="/change-password" element={<PwChange />} />
+              <Route path="/points" element={<Points />} />
+              <Route path="/collection" element={<Collection />} />
+              <Route path="/admin" element={<Admin />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
