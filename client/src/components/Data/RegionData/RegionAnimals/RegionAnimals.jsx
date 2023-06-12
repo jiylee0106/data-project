7ee3bf0e5d2f4a311da762424b3862912cd560bb
이승현 <billlee2656@gmail.com>
2023-06-13 02:00:39 +0900

    feat: MR용

--- client/src/components/Data/RegionData/RegionAnimals/RegionAnimals.jsx
+++ client/src/components/Data/RegionData/RegionAnimals/RegionAnimals.jsx
@@ -27,7 +27,7 @@ const RegionAnimals = ({ region, speciesCount }) => {
           );
         })}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
         <RegionAnimalsList region={region} species={species} />
       </div>
     </>
--- client/src/components/Data/RegionData/RegionAnimals/RegionAnimalsList.jsx
+++ client/src/components/Data/RegionData/RegionAnimals/RegionAnimalsList.jsx
@@ -67,8 +67,8 @@ const RegionAnimalsList = ({ region, species }) => {
         />
       ))}
 
-      <nav aria-label="Page navigation example">
-        <ul className="inline-flex items-center -space-x-px">
+      <nav>
+        <ul className="flex">
           <li>
             <button
               onClick={handlePrevClick}
@@ -100,7 +100,13 @@ const RegionAnimalsList = ({ region, species }) => {
             <li key={pageNumber}>
               <button
                 onClick={() => handlePageChange(pageNumber)}
-                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                className={`px-3 py-2 leading-tight border hover:bg-gray-100 hover:text-gray-700 
+                  ${
+                    pageNumber === currentPage
+                      ? "text-blue-600 bg-neutral-300"
+                      : "text-gray-500 bg-white"
+                  }
+                  dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
               >
                 {pageNumber}
               </button>
