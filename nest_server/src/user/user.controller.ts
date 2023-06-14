08454bafd87d157b606d6baef4ee05ce47a183a2
이승현 <billlee2656@gmail.com>
2023-06-15 03:34:07 +0900

    feat: 유저 서비스 구축

--- nest_server/src/user/user.controller.ts
+++ nest_server/src/user/user.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/passport/jwt.guard';
+import { RequestUser } from '../auth/interfaces/RequestUser.interface';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { GetUserResponse } from '../swagger/user.swagger';
+import { UserPasswordrDto } from './user.dto';
+import { MessageResponse } from '../swagger/global.swagger';
 
+@ApiTags('User')
 @Controller('user')
-export class UserController {}
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @ApiOperation({ summary: '유저 정보 제공' })
+  @ApiResponse(GetUserResponse)
+  async getUser(@Req() req: RequestUser) {
+    const result = await this.userService.getUser(req.user.email);
+    return result;
+  }
+
+  @ApiOperation({ summary: '유저 정보 업데이트' })
+  @ApiBody({
+    description: '업데이트 요청 정보',
+    type: UserPasswordrDto,
+  })
+  @ApiResponse(MessageResponse(200, '유저 업데이트 성공'))
+  @Patch()
+  async changePassword(
+    @Req() req: RequestUser,
+    @Body('password') password: string,
+  ) {
+    await this.userService.changePassword(req.user.id, password);
+    return { message: '유저 정보 업데이트 성공' };
+  }
+
+  @ApiOperation({ summary: '유저 삭제' })
+  @ApiResponse(MessageResponse(200, '유저 삭제 성공'))
+  @Delete()
+  async deleteUser(@Req() req: RequestUser) {
+    await this.userService.deleteUser(req.user.id);
+    return { message: '유저 삭제 성공' };
+  }
+}
--- nest_server/src/user/user.service.ts
+++ nest_server/src/user/user.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserRepository } from '../user/user.repository';
+import { HandlePassword } from '../libraries/integrations/HandlePassword';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly handlePassword: HandlePassword,
+  ) {}
+
+  getUser = async (email: string) => {
+    const user = await this.userRepository.getUserByEmail(email);
+    if (!user) {
+      throw new NotFoundException('유저 조회 실패');
+    }
+    return { email: user.email, role: user.provider };
+  };
+
+  changePassword = async (id: number, password: string) => {
+    const hashedPassword = await this.handlePassword.hashPassword(password);
+    return await this.userRepository.changePassword(id, hashedPassword);
+  };
+
+  deleteUser = async (id: number) => {
+    return await this.userRepository.delete(id);
+  };
+}
