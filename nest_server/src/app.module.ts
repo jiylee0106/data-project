61cea2034997cd30d85a5e401ee4ce7ebd09b323
이승현 <billlee2656@gmail.com>
2023-06-15 00:50:23 +0900

    feat: nest 모듈 구성

--- nest_server/src/app.module.ts
+++ nest_server/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PointModule } from './point/point.module';
+import { ContentModule } from './content/content.module';
+import { CollectionModule } from './collection/collection.module';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { CampaignModule } from './campaign/campaign.module';
+import { NewsModule } from './news/news.module';
+import { ParticipationModule } from './participation/participation.module';
+import { VideoModule } from './video/video.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    PointModule,
+    ContentModule,
+    CollectionModule,
+    AuthModule,
+    AdminModule,
+    CampaignModule,
+    NewsModule,
+    ParticipationModule,
+    VideoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
