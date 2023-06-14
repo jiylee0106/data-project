0a142eee0fb43cf15cab6a85ae23b638a16495a5
이승현 <billlee2656@gmail.com>
2023-06-15 02:16:56 +0900

    feat: Auth 모듈 및 비밀번호 핸들링 로직 구성

--- nest_server/src/auth/auth.module.ts
+++ nest_server/src/auth/auth.module.ts
@@ -7,14 +7,31 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './passport/jwt.strategy';
 import { UserModule } from '../user/user.module';
 import { LocalStrategy } from './passport/local.strategy';
-import { RedisModule } from '../redis/redis.module';
-import { HandlePassword } from '../integrations/handlePassword';
-import { GoogleStrategy } from './passport/google.strategy';
-import { KakaoStrategy } from './passport/kakao.strategy';
+import { HandlePassword } from '../libraries/integrations/HandlePassword';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<JwtModuleOptions> => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '14d' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    PrismaService,
+    LocalStrategy,
+    HandlePassword,
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
