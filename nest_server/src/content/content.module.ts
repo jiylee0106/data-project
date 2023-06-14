3b60834c2a253a26329f09935b09da98cc76af62
이승현 <billlee2656@gmail.com>
2023-06-15 01:06:33 +0900

    feat: nest 리포지토리 구성

--- nest_server/src/content/content.module.ts
+++ nest_server/src/content/content.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContentService } from './content.service';
+import { ContentController } from './content.controller';
 
 @Module({
-  providers: [ContentService]
+  providers: [ContentService],
+  controllers: [ContentController]
 })
 export class ContentModule {}
