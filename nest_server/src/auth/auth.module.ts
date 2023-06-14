6fbba49918b32d611afa1e6de4d5cfc842eafc1a
이승현 <billlee2656@gmail.com>
2023-06-15 02:05:06 +0900

    feat: nest 가드 및 jwt,local전략 구성

--- nest_server/src/auth/auth.module.ts
+++ nest_server/src/auth/auth.module.ts
@@ -4,6 +4,14 @@ import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma.service';
 import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './passport/jwt.strategy';
+import { UserModule } from '../user/user.module';
+import { LocalStrategy } from './passport/local.strategy';
+import { RedisModule } from '../redis/redis.module';
+import { HandlePassword } from '../integrations/handlePassword';
+import { GoogleStrategy } from './passport/google.strategy';
+import { KakaoStrategy } from './passport/kakao.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
