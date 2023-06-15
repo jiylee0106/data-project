41636fff1db86f01a2579fc6bfe2124e03e5714e
이승현 <billlee2656@gmail.com>
2023-06-15 16:42:07 +0900

    feat: 컨텐츠 제공 컨트롤러 라우팅 구성

--- nest_server/src/content/content.controller.ts
+++ nest_server/src/content/content.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ContentService } from './content.service';
 
 @Controller('content')
-export class ContentController {}
+export class ContentController {
+  constructor(private readonly contentService: ContentService) {}
+
+  @Get('/video')
+  async getSelectedVideos() {
+    const result = await this.contentService.getSelectedVideoService();
+    return result;
+  }
+
+  @Get('/participation')
+  async getSelectedParticipation() {
+    const result = await this.contentService.getSelectedParticipationService();
+    return result;
+  }
+
+  @Get('/news')
+  async getNews() {
+    const result = await this.contentService.getNewsService();
+    return result;
+  }
+
+  @Get('/campaign')
+  async getCampaigns() {
+    const result = await this.contentService.getCampaignService();
+    return result;
+  }
+}
