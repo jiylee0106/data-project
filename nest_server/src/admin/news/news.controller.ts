fa313bce6a5ed88cef5418ca900153a29fdb3516
이승현 <billlee2656@gmail.com>
2023-06-15 17:59:55 +0900

    feat: 뉴스 응답 DTO설정 및 컨트롤러 적용

--- nest_server/src/admin/news/news.controller.ts
+++ nest_server/src/admin/news/news.controller.ts
@@ -11,6 +11,12 @@ import {
 import { AdminGuard } from '../../auth/passport/admin.guard';
 import { JwtAuthGuard } from '../../auth/passport/jwt.guard';
 import { NewsService } from './news.service';
+import { GetNewsResponseDto } from './dto/news.response.dto';
+import { MessageResponseDto } from '../../app.dto';
+import {
+  CreateNewsRequestDto,
+  UpdateNewsRequestDto,
+} from './dto/news.request.dto';
 
 @Controller('admin/news')
 @UseGuards(JwtAuthGuard, AdminGuard)
@@ -18,22 +24,27 @@ export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
   @Get()
-  async getNews() {
+  async getNews(): Promise<GetNewsResponseDto[]> {
     return this.newsService.getNewsService();
   }
 
   @Post()
-  async createNews(@Body() body: any) {
+  async createNews(
+    @Body() body: CreateNewsRequestDto,
+  ): Promise<MessageResponseDto> {
     return this.newsService.putNewsService(body);
   }
 
   @Patch(':id')
-  async updateNews(@Param('id') id: string, @Body() body: any) {
+  async updateNews(
+    @Param('id') id: string,
+    @Body() body: UpdateNewsRequestDto,
+  ): Promise<MessageResponseDto> {
     return this.newsService.patchNewsService(Number(id), body);
   }
 
   @Delete(':id')
-  async deleteNews(@Param('id') id: string) {
+  async deleteNews(@Param('id') id: string): Promise<MessageResponseDto> {
     return this.newsService.deleteNewsService(Number(id));
   }
 }
