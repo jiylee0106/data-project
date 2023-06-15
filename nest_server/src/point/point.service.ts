8eb7adec371a188a379952e3237ebf57bf3c4ad3
이승현 <billlee2656@gmail.com>
2023-06-15 19:57:36 +0900

    feat: 포인트 로그 응답 DTO 서비스 구성 완료

--- nest_server/src/point/point.service.ts
+++ nest_server/src/point/point.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { PointRepository } from './point.repository';
 import HandlePoint from '../libraries/integrations/HandlePoints';
 import { PointsLog } from '@prisma/client';
+import {
+  GetAllPointLogsResponseDto,
+  GetCampaignLogsResponseDto,
+  GetDailyEventsLogsResponseDto,
+  GetPointResponseDto,
+} from './dto/point.response.dto';
+import { MessageResponseDto } from '../app.dto';
 
 @Injectable()
 export class PointService {
@@ -10,31 +17,33 @@ export class PointService {
     private readonly handlePoint: HandlePoint,
   ) {}
 
-  async getPointService(user_id: number) {
+  async getPointService(user_id: number): Promise<GetPointResponseDto> {
     const point = await this.handlePoint.getPoints(user_id);
     return { point };
   }
 
-  async getAllLogService(user_id: number): Promise<{ logs: PointsLog[] }> {
+  async getAllLogService(user_id: number): Promise<GetAllPointLogsResponseDto> {
     const logs = await this.pointRepository.getPointsLog(user_id);
     return { logs };
   }
 
-  async getCampaignLogService(user_id: number): Promise<{ logs: PointsLog[] }> {
+  async getCampaignLogService(
+    user_id: number,
+  ): Promise<GetCampaignLogsResponseDto> {
     const logs = await this.pointRepository.getCampaignLog(user_id);
     return { logs };
   }
 
   async getDailyEventsLogService(
     user_id: number,
-  ): Promise<{ logs: PointsLog[] }> {
+  ): Promise<GetDailyEventsLogsResponseDto> {
     const logs = await this.pointRepository.getDailyEventsLog(user_id);
     return { logs };
   }
 
   async putPointService(
     data: Pick<PointsLog, 'userId' | 'action_type' | 'method'>,
-  ) {
+  ): Promise<MessageResponseDto> {
     if (data.action_type === 'Earned') {
       return await this.handlePoint.earnPoints(data);
     } else {
