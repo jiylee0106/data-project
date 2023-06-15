2f2f7bb752abc2675d63c1d1d6a1640fdc7d8363
이승현 <billlee2656@gmail.com>
2023-06-15 19:38:18 +0900

    feat: 컨텐츠 제공 응답 DTO 컨트롤러 적용

--- nest_server/src/content/content.controller.ts
+++ nest_server/src/content/content.controller.ts
@@ -1,30 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { ContentService } from './content.service';
+import { GetVideoResponseDto } from 'src/admin/video/dto/video.response.dto';
+import { GetParticipationResponseDto } from 'src/admin/participation/dto/participation.response.dto';
+import { GetNewsResponseDto } from 'src/admin/news/dto/news.response.dto';
+import { GetCampaignResponseDto } from 'src/admin/campaign/dto/campaign.response.dto';
 
 @Controller('content')
 export class ContentController {
   constructor(private readonly contentService: ContentService) {}
 
   @Get('/video')
-  async getSelectedVideos() {
+  async getSelectedVideos(): Promise<GetVideoResponseDto> {
     const result = await this.contentService.getSelectedVideoService();
     return result;
   }
 
   @Get('/participation')
-  async getSelectedParticipation() {
+  async getSelectedParticipation(): Promise<GetParticipationResponseDto> {
     const result = await this.contentService.getSelectedParticipationService();
     return result;
   }
 
   @Get('/news')
-  async getNews() {
+  async getNews(): Promise<GetNewsResponseDto[]> {
     const result = await this.contentService.getNewsService();
     return result;
   }
 
   @Get('/campaign')
-  async getCampaigns() {
+  async getCampaigns(): Promise<GetCampaignResponseDto[]> {
     const result = await this.contentService.getCampaignService();
     return result;
   }
