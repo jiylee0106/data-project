2b31d07f0e0764e777cb7c323710a9b472fe32c5
이승현 <billlee2656@gmail.com>
2023-06-15 19:44:41 +0900

    feat: 유저 서비스 응답 DTO 설정 완료

--- nest_server/src/user/user.service.ts
+++ nest_server/src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from '../user/user.repository';
 import { HandlePassword } from '../libraries/integrations/HandlePassword';
+import { GetUserResponseDto } from './dto/user.response.dto';
+import { MessageResponseDto } from '../app.dto';
 
 @Injectable()
 export class UserService {
@@ -9,7 +11,7 @@ export class UserService {
     private readonly handlePassword: HandlePassword,
   ) {}
 
-  getUser = async (email: string) => {
+  getUser = async (email: string): Promise<GetUserResponseDto> => {
     const user = await this.userRepository.getUserByEmail(email);
     if (!user) {
       throw new NotFoundException('유저 조회 실패');
@@ -17,13 +19,16 @@ export class UserService {
     return { email: user.email, role: user.role };
   };
 
-  changePassword = async (id: number, password: string) => {
+  changePassword = async (
+    id: number,
+    password: string,
+  ): Promise<MessageResponseDto> => {
     const hashedPassword = await this.handlePassword.hashPassword(password);
     await this.userRepository.changePassword(id, hashedPassword);
     return { message: '비밀번호 변경 성공' };
   };
 
-  deleteUser = async (id: number) => {
+  deleteUser = async (id: number): Promise<MessageResponseDto> => {
     await this.userRepository.delete(id);
     return { message: '유저 삭제 성공' };
   };
