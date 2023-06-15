f5ac77ff4e9d4c63225aa4ad0022413c5e2a2191
이승현 <billlee2656@gmail.com>
2023-06-15 15:15:34 +0900

    feat: 뉴스 리포완성

--- nest_server/src/news/news.repository.ts
+++ nest_server/src/news/news.repository.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { News } from '@prisma/client';
 
 @Injectable()
-export class NewsRepository {}
+export class NewsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async putNews(
+    news: Pick<News, 'title' | 'description' | 'link' | 'image_link'>,
+  ): Promise<void> {
+    await this.prisma.news.create({ data: news });
+  }
+
+  async patchNews(
+    id: number,
+    data: Pick<News, 'title' | 'description' | 'link' | 'image_link'>,
+  ): Promise<void> {
+    const { title, description, link, image_link } = data;
+    await this.prisma.news.update({
+      where: { id },
+      data: { title, description, link, image_link },
+    });
+  }
+
+  async deleteNews(id: number): Promise<void> {
+    await this.prisma.news.delete({ where: { id } });
+  }
+}
