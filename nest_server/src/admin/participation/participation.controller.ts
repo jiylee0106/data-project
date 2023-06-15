382bc8a9a36ba5e13dbe7e7d7dfadbe62aaf0002
이승현 <billlee2656@gmail.com>
2023-06-15 18:03:27 +0900

    feat: 동참 컨트롤러 DTO적용 완료

--- nest_server/src/admin/participation/participation.controller.ts
+++ nest_server/src/admin/participation/participation.controller.ts
@@ -11,6 +11,12 @@ import {
 import { AdminGuard } from '../../auth/passport/admin.guard';
 import { JwtAuthGuard } from '../../auth/passport/jwt.guard';
 import { ParticipationService } from './participation.service';
+import { GetParticipationResponseDto } from './dto/participation.response.dto';
+import { MessageResponseDto } from '../../app.dto';
+import {
+  CreateParticipationRequestDto,
+  UpdateParticipationRequestDto,
+} from './dto/participation.request.dto';
 
 @Controller('admin/participation')
 @UseGuards(JwtAuthGuard, AdminGuard)
@@ -18,17 +24,22 @@ export class ParticipationController {
   constructor(private readonly participationService: ParticipationService) {}
 
   @Get()
-  async getParticipation() {
+  async getParticipation(): Promise<GetParticipationResponseDto[]> {
     return this.participationService.getParticipationService();
   }
 
   @Post()
-  async createParticipation(@Body() body: any) {
+  async createParticipation(
+    @Body() body: CreateParticipationRequestDto,
+  ): Promise<MessageResponseDto> {
     return this.participationService.putParticipationService(body);
   }
 
   @Patch('patch/:id')
-  async updateParticipation(@Param('id') id: string, @Body() body: any) {
+  async updateParticipation(
+    @Param('id') id: string,
+    @Body() body: UpdateParticipationRequestDto,
+  ): Promise<MessageResponseDto> {
     return this.participationService.patchParticipationService(
       Number(id),
       body,
@@ -36,12 +47,16 @@ export class ParticipationController {
   }
 
   @Patch('set-current/:id')
-  async setCurrentParticipation(@Param('id') id: string) {
+  async setCurrentParticipation(
+    @Param('id') id: string,
+  ): Promise<MessageResponseDto> {
     return this.participationService.setCurrentParticipationService(Number(id));
   }
 
   @Delete(':id')
-  async deleteParticipation(@Param('id') id: string) {
+  async deleteParticipation(
+    @Param('id') id: string,
+  ): Promise<MessageResponseDto> {
     return this.participationService.deleteParticipationService(Number(id));
   }
 }
