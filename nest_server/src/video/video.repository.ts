b22ce999828eb142c9fffdf16a89429109778443
이승현 <billlee2656@gmail.com>
2023-06-15 15:46:14 +0900

    feat: 어드민 비디오 리포 완료

--- nest_server/src/video/video.repository.ts
+++ nest_server/src/video/video.repository.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Video } from '@prisma/client';
 
 @Injectable()
-export class VideoRepository {}
+export class VideoRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async putVideo(
+    video: Pick<Video, 'video_id' | 'title' | 'description'>,
+  ): Promise<void> {
+    await this.prisma.video.create({ data: video });
+  }
+
+  async setCurrentVideo(id: number): Promise<void> {
+    await this.prisma.$transaction([
+      this.prisma.video.updateMany({
+        where: { is_selected: 1 },
+        data: { is_selected: 0 },
+      }),
+      this.prisma.video.update({
+        where: { id },
+        data: { is_selected: 1 },
+      }),
+    ]);
+  }
+
+  async getVideo(): Promise<Video[]> {
+    const result = await this.prisma.video.findMany({
+      orderBy: {
+        id: 'desc',
+      },
+    });
+
+    return result;
+  }
+
+  async patchVideo(
+    id: number,
+    video: Pick<Video, 'video_id' | 'title' | 'description'>,
+  ): Promise<void> {
+    const { video_id, title, description } = video;
+    await this.prisma.video.update({
+      where: { id },
+      data: { video_id, title, description },
+    });
+  }
+
+  async deleteVideo(id: number): Promise<void> {
+    await this.prisma.video.delete({ where: { id } });
+  }
+}
