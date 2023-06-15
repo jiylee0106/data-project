18d50b6dc0ede334aa12c9bb041577e7e1bff694
Chaeyoung <codudghkdlxld@gmail.com>
2023-06-15 23:37:17 +0900

    design. 자료 페이지 디자인 수정

--- client/src/components/Participate/Quiz/QuizProblem.jsx
+++ client/src/components/Participate/Quiz/QuizProblem.jsx
@@ -23,7 +23,7 @@ const QuizProblem = ({
         이 멸종위기 야생생물의 이름은?
       </h3>
 
-      <ul className="w-full  font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+      <ul className="w-full font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         {randomOptions.map((option, index) => (
           <li
             key={index}
@@ -35,7 +35,7 @@ const QuizProblem = ({
                 type="radio"
                 value={option}
                 name="list-radio"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                className="w-4 h-4 text-[#85B7CC] bg-[#85B7CC] border-gray-300 focus:ring-[#85B7CC] dark:focus:ring-[#85B7CC] dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                 checked={selectedOption === option}
                 onChange={handleOptionChange}
               />
