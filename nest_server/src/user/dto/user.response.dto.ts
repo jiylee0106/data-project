746f7371f1eaa3fb5343c872908b0d006d966a08
이승현 <billlee2656@gmail.com>
2023-06-16 01:42:50 +0900

    feat: 관리자 유저 제어 로직 추가

--- nest_server/src/user/dto/user.response.dto.ts
+++ nest_server/src/user/dto/user.response.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Role } from '@prisma/client';
-import { IsEnum, IsEmail } from 'class-validator';
+import { Provider, Role } from '@prisma/client';
+import { IsEnum, IsEmail, IsInt, IsDate } from 'class-validator';
 
 class GetUserResponseDto {
   @ApiProperty()
@@ -12,4 +12,26 @@ class GetUserResponseDto {
   role: Role;
 }
 
-export { GetUserResponseDto };
+class GetAllUsersResponseDto {
+  @ApiProperty()
+  @IsInt()
+  id: number;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsEnum(Provider)
+  provider: Provider;
+
+  @ApiProperty()
+  @IsEnum(Role)
+  role: Role;
+
+  @ApiProperty()
+  @IsDate()
+  created_at: Date;
+}
+
+export { GetUserResponseDto, GetAllUsersResponseDto };
