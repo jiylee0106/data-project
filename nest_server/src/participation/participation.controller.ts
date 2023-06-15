70a46b1dd0f2a90d7b8134d7c1d39ff4d3a61e8e
이승현 <billlee2656@gmail.com>
2023-06-15 15:40:00 +0900

    feat: 동참하기 라우팅 컨트롤러 구성 완료

--- nest_server/src/participation/participation.controller.ts
+++ nest_server/src/participation/participation.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AdminGuard } from '../auth/passport/admin.guard';
+import { JwtAuthGuard } from '../auth/passport/jwt.guard';
+import { ParticipationService } from './participation.service';
 
+@Controller('admin/news')
+@UseGuards(JwtAuthGuard, AdminGuard)
 @Controller('participation')
-export class ParticipationController {}
+export class ParticipationController {
+  constructor(private readonly participationService: ParticipationService) {}
+
+  @Get()
+  async getParticipation() {
+    return this.participationService.getParticipationService();
+  }
+
+  @Post()
+  async createParticipation(@Body() body: any) {
+    return this.participationService.putParticipationService(body);
+  }
+
+  @Patch('patch/:id')
+  async updateParticipation(@Param('id') id: string, @Body() body: any) {
+    return this.participationService.patchParticipationService(
+      Number(id),
+      body,
+    );
+  }
+
+  @Patch('set-current/:id')
+  async setCurrentParticipation(@Param('id') id: string) {
+    return this.participationService.setCurrentParticipationService(Number(id));
+  }
+
+  @Delete(':id')
+  async deleteNews(@Param('id') id: string) {
+    return this.participationService.deleteParticipationService(Number(id));
+  }
+}
