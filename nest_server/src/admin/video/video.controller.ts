cddfcf4d0c3177d7e4bcd69b013cfbbc7f0f919d
이승현 <billlee2656@gmail.com>
2023-06-15 19:14:02 +0900

    feat: video dto 요청및 응답 컨트롤러에 설정

--- nest_server/src/admin/video/video.controller.ts
+++ nest_server/src/admin/video/video.controller.ts
@@ -11,6 +11,12 @@ import {
 import { AdminGuard } from '../../auth/passport/admin.guard';
 import { JwtAuthGuard } from '../../auth/passport/jwt.guard';
 import { VideoService } from './video.service';
+import { GetVideoResponseDto } from './dto/video.response.dto';
+import {
+  CreateVideoRequestDto,
+  UpdateVideoRequestDto,
+} from './dto/video.request.dto';
+import { MessageResponseDto } from '../../app.dto';
 
 @Controller('admin/video')
 @UseGuards(JwtAuthGuard, AdminGuard)
@@ -18,27 +24,32 @@ export class VideoController {
   constructor(private readonly videoService: VideoService) {}
 
   @Get()
-  async getVideo() {
+  async getVideo(): Promise<GetVideoResponseDto[]> {
     return this.videoService.getVideoService();
   }
 
   @Post()
-  async createVideo(@Body() body: any) {
+  async createVideo(
+    @Body() body: CreateVideoRequestDto,
+  ): Promise<MessageResponseDto> {
     return this.videoService.putVideoService(body);
   }
 
   @Patch('patch/:id')
-  async updateVideo(@Param('id') id: string, @Body() body: any) {
+  async updateVideo(
+    @Param('id') id: string,
+    @Body() body: UpdateVideoRequestDto,
+  ): Promise<MessageResponseDto> {
     return this.videoService.patchVideoService(Number(id), body);
   }
 
   @Patch('set-current/:id')
-  async setCurrentVideo(@Param('id') id: string) {
+  async setCurrentVideo(@Param('id') id: string): Promise<MessageResponseDto> {
     return this.videoService.setCurrentVideoService(Number(id));
   }
 
   @Delete(':id')
-  async deleteVideo(@Param('id') id: string) {
+  async deleteVideo(@Param('id') id: string): Promise<MessageResponseDto> {
     return this.videoService.deleteVideoService(Number(id));
   }
 }
